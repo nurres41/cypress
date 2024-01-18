3b3820e956c8282772797f053bd363abdb0b88a7
nurionurkurtulus <nurionurkurtulus@gmail.com>
2024-01-18 23:11:08 +0100

    config,query,command and task

---
+++ cypressThird/cypress/e2e/contact.cy.js
@@ -0,0 +1,72 @@
+/// <reference types="Cypress" />
+
+describe('contact form', () => {
+
+  // Baslamadan once test sayfaya gidiyor. 
+  // Her testimiz bu sayfaya gittigi icin tekrarlamanin onune beforeEach kullanarak gecebiliriz.
+
+  beforeEach(() => {
+    cy.visit('/about');
+  })
+
+  it('should submit the form', () => {
+    cy.getById('contact-input-message').type('Hello world!');
+    cy.getById('contact-input-name').type('John Doe');
+    cy.getById('contact-btn-submit').then((el) => {
+      expect(el.attr('disabled')).to.be.undefined;
+      expect(el.text()).to.eq('Send Message');
+    });
+    cy.screenshot();
+    cy.getById('contact-input-email').type('test@example.com');
+    cy.submitForm();
+    // cy.get('[data-cy="contact-btn-submit"]')
+    //   .contains('Send Message')
+    //   .should('not.have.attr', 'disabled');
+    cy.screenshot();
+    cy.getById('contact-btn-submit').as('submitBtn');
+    // cy.get('@submitBtn').click();
+    cy.get('@submitBtn').contains('Sending...');
+    cy.get('@submitBtn').should('have.attr', 'disabled');
+  });
+
+
+  it('should validate the form input', () => {
+    cy.submitForm();
+    cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+      expect(el).to.not.have.attr('disabled');
+      expect(el.text()).to.not.equal('Sending...');
+    });
+    cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
+    cy.get('[data-cy="contact-input-message"]').as('msgInput');
+    cy.get('@msgInput').focus().blur();
+    cy.get('@msgInput')
+      .parent()
+      .should((el) => {
+        expect(el.attr('class')).not.to.be.undefined;
+        expect(el.attr('class')).contains('invalid');
+      })
+
+    cy.get('[data-cy="contact-input-name"]').focus().blur();
+    cy.get('[data-cy="contact-input-name"]')
+      .parent()
+      .should((el) => {
+        expect(el.attr('class')).not.to.be.undefined;
+        expect(el.attr('class')).contains('invalid');
+      })
+
+    cy.get('[data-cy="contact-input-email"]').focus().blur();
+    cy.get('[data-cy="contact-input-email"]')
+      .parent()
+      .should((el) => {
+        expect(el.attr('class')).not.to.be.undefined;
+        expect(el.attr('class')).contains('invalid');
+      })
+  });
+});
+
+
+
+// before() Bir kere butun testler baslamadan once calisir
+// beforeEach() Her test baslamadan once calisir. Repeated
+// afterEach() her test bittiginde calisir
+// after() butun testler bittiginde calisir
